eaa82f11a22877630bebcc4191a2ae4ef2e90300
Soberanes <paulsoberanes@gmail.com>
2015-09-29 18:45:41 -0500

    Admin panel

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,7 +2,17 @@
 class AdminController extends BaseController {
 
     public function index(){
-        
+
+        $contacts_count = Contact::count();
+        $users_count = User::count();
+
+        $data = array(
+            "contacts_count" => $contacts_count,
+            "contacts_count" => $contacts_count,
+        );
+
+
+        return View::make('admin.index', $data);
     }
 
 }
--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -1,1 +1,24 @@
 @extends('layouts.master')
+
+@section('sidebar')
+    @parent
+@stop
+
+@section('content')
+    <hgroup class="wrap">
+        <h1>Panel de administración</h1>
+    </hgroup>
+    <section class="wrap">
+        <ul class="list">
+            <li>
+                <h2>Resumen</h2>
+                <h3>Hay {{ $contacts_count }} contactos.</h3>
+                <h3>Hay 0 páginas</h3>
+                <h3>Hay {{ $users_count }} usuarios</h3>
+                <p class="lead">
+                    Agrega un nuevo contacto en el botón <span>Agregar nuevo contacto</span>.
+                </p>
+            </li>
+        </ul>
+    </section>
+@stop
